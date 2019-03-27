ff13bdb8e5d7dffa9bd314170f4440d3f157c6c0
François Boulogne <devel@sciunto.org>
2019-03-27 13:16:16 +0100

    cleanup

--- viewer.py
+++ viewer.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 27 11:45:18 2019
-
-@author: Administrateur
-"""
-
 from pymba import Vimba
 from display_frame import display_frame
 
@@ -18,16 +12,12 @@ if __name__ == '__main__':
         camera.arm('SingleFrame')
 
         acquiring = True
-
-        # capture a single frame, more than once if desired
         while acquiring:
             frame = camera.acquire_frame()
             try:
                 display_frame(frame, 0)
             except StopIteration:
                 acquiring = False
-                
-                
-        camera.disarm()
 
+        camera.disarm()
         camera.close()
